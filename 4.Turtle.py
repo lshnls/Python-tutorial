13f50daf7d3eb5b3e5f1e95c47bb8b40222d3a75
Lysuhin Ilya <lshnls@ya.ru>
2024-08-05 19:56:12 +0300

    new file:   4.Turtle.py 	modified:   README.md

---
+++ 4.Turtle.py
@@ -0,0 +1,20 @@
+# Первый урок программирования на Phyton
+
+import turtle
+window = turtle.Screen()
+t = turtle.Pen()
+#t.reset()
+t.forward(50)
+#t.up()
+t.left(90)
+t.forward(50)
+#t.down()
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+#input("Press any key to exit ...")
+
+
+window.exitonclick() # ждем нажатия кнопки
